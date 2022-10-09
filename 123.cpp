e7f83375bdce9a4145f72f34ba5dc8df51939feb
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-10-09 16:39:43 +0300

    решение

---
+++ 123.cpp
@@ -0,0 +1,16 @@
+﻿//Oleg
+//09.10.22
+//L1
+
+#include <iostream>
+using namespace std;
+int main() {
+	cout << " hello me!";
+	int a;
+	cin >> a;
+	int fd, sd;
+	sd = a % 10;
+	sd *= 10;
+	cout << sd;
+	cout << &a <<endl;
+}
